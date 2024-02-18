ecd39c8d8e73d76054a2bd6674b0e19c647fd8d8
Rasya Hidayat <naufalrasya21907@gmail.com>
2024-02-18 17:12:37 +0700

    Idle Animation
    Ngetest sama versi error apa engga

--- DPK A PAS/Assets/Script/PlayerMovement.cs
+++ DPK A PAS/Assets/Script/PlayerMovement.cs
@@ -19,7 +19,17 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        body.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector3(HorizontalInput * speed, body.velocity.y);
+        
+        //flip player when move left and right
+        if(HorizontalInput > 0.01f){
+            transform.localScale = new Vector3(1.5f,1.5f,1);
+        }
+
+        if(HorizontalInput < -0.01f){
+            transform.localScale = new Vector3(-1.5f,1.5f,1);
+        }
 
         if(Input.GetKey(KeyCode.Space)){
             body.velocity = new Vector2(body.velocity.x, speed);
