c8b8b334e87a7e19bced49752ae31ed16f002c2e
Farrel44 <farrellasso69@gmail.com>
2024-04-30 08:04:20 +0700

    Enemy Mechanic ( not fixed )

---
+++ DPK A PAS/Assets/Script/EnemyControl.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyControl : MonoBehaviour
+{   
+    public GameObject PointA1;
+    public GameObject PointA2;
+    private Rigidbody2D rb2;
+    private Animator anim;
+    private Transform CurrentPoint;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        CurrentPoint = PointA2.transform;
+        anim.SetBool("Is_Running", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 point = CurrentPoint.position - transform.position;
+        if(CurrentPoint == PointA2.transform){
+            rb2.velocity = new Vector2(speed, 0);
+        }
+        else{
+            rb2.velocity = new Vector2(-speed, 0);
+        }
+
+        
+
+        if(Vector2.Distance(transform.position, CurrentPoint.position) < 0.5f && (CurrentPoint == PointA2.transform)){
+            CurrentPoint = PointA1.transform;
+        }
+        if(Vector2.Distance(transform.position, CurrentPoint.position) < 0.5f && (CurrentPoint == PointA1.transform)){
+            CurrentPoint = PointA2.transform;
+        }
+    }
+}
