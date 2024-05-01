ca09966a8b207485f69d083e57204075597058cb
Farrel44 <farrellasso69@gmail.com>
2024-05-01 11:38:13 +0700

    Enemy Animation Fix & Enemy Collision Script

--- DPK A PAS/Assets/Script/EnemyControl.cs
+++ DPK A PAS/Assets/Script/EnemyControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Drawing;
 using UnityEngine;
 
 public class EnemyControl : MonoBehaviour
@@ -34,10 +35,25 @@ public class EnemyControl : MonoBehaviour
         
 
         if(Vector2.Distance(transform.position, CurrentPoint.position) < 0.5f && (CurrentPoint == PointA2.transform)){
+            flip();
             CurrentPoint = PointA1.transform;
         }
         if(Vector2.Distance(transform.position, CurrentPoint.position) < 0.5f && (CurrentPoint == PointA1.transform)){
+            flip();
             CurrentPoint = PointA2.transform;
         }
+
     }
+
+    private void flip(){
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnDrawGizmos(){
+            Gizmos.DrawWireSphere(PointA1.transform.position, 0.5f);
+            Gizmos.DrawWireSphere(PointA2.transform.position, 0.5f);
+            Gizmos.DrawLine(PointA1.transform.position, PointA2.transform.position);
+        }    
 }
