687a1113bbbb031aedcef4eda7ef1b8313518097
Rasyaa21 <naufalrasya21907@gmail.com>
2024-05-06 14:19:11 +0700

    ..
    ..

--- DPK A PAS/Assets/Script/pauseMEnu.cs
+++ DPK A PAS/Assets/Script/pauseMEnu.cs
@@ -11,22 +11,26 @@ public class pauseMEnu : MonoBehaviour
     public void Pause(){
         pauseMenu.SetActive(true);
         Time.timeScale = 0;
+        Debug.Log("Pause");
     }
 
     public void Resume(){
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
+        Debug.Log("Resume");
     }
 
     public void Home(){
         SceneManager.LoadScene("Level Select");
         Time.timeScale = 1;
+        Debug.Log("Home");
     }
 
     public void Restart(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         Time.timeScale = 1;
         pauseMenu.SetActive(false);
+        Debug.Log("Restart");
     }
 
 
